87924b6600a609d5294cf1273eb5c754e35851b4
AISHIK ADHIKARY <aishikadhikary154@gmail.com>
2022-12-04 01:02:44 +0530

    New commit

---
+++ compare.js
@@ -0,0 +1,28 @@
+x=7;
+
+console.log(x>9);
+
+
+console.log(x==7); //lose equality operater
+
+console.log(x===7);  //strict equality operator
+
+console.log(x!==7);
+
+console.log(x!=7);
+
+console.log("78"==78);
+console.log("78"===78);
+
+//-------------------------------------------------------//
+
+let y=x>7 ? "good":"bad"; // Ternary operator
+
+console.log(y);
+
+function sog(z){
+   a=z>10?"gold":"silver";
+   return a;
+}
+
+console.log(sog(45));
